c0be5854b9547d0aa2e7f26d83785e65b966261a
Steven Normore <snormore@gmail.com>
2020-07-07 18:28:18 -0400

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello! you've requested %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Hello - you've requested %s\n", r.URL.Path)
 	})
 
 	port := os.Getenv("PORT")
